33ab682b7b8a9b7afbe1e56f5f2b5d616d64ba7a
rupalikiran1 <rupalikiran1999@gmail.com>
2024-02-18 20:57:52 +0530

    added

---
+++ script9.js
@@ -0,0 +1,74 @@
+//push
+//action
+//return type
+
+let names = ["krupali","kiran","vaibhav","ram","pankaj","omkar"]
+let q1 = names.push("shambhu")
+console.log(q1)
+
+// unshift()
+let q2 = names.unshift("manisha")
+console.log(q2)
+console.log(names)
+
+// pop()
+let fruits = ["apple","mango","banana","grapes"]
+let q3 = fruits.pop()
+console.log(q3)
+console.log(fruits)
+
+// shift()
+let q4 = fruits.shift()
+console.log(q4)
+console.log(fruits)
+
+//indexof()
+let animals = ["tiger","lion","bear","cat"]
+let q5 = animals.indexOf("lion")
+console.log(q5)
+let q6 = animals.indexOf("Lion")
+console.log(q6)
+
+// includes()
+let city = ["banglore","kolkata","chennai","nagpur"]
+let q7 = city.includes("chennai")
+console.log(q7)
+
+// reverser()
+let q8 = city.reverse()
+console.log(city)
+console.log(q8)
+
+// at()
+let country = ["india","austrlia","bangladesh","srilanka"]
+let q9 = country.at(2)
+console.log(q9)
+
+// concat()
+let a = [11,22,33]
+let b = [44,55,66]
+
+let q10 = a.concat(b)
+console.log(q10)
+console.log(a)
+console.log(b)
+
+// join()
+
+let info = ["krupali","kotkar",7744087820]
+let q11 = info.join("-")
+let q12 = info.join("@")
+console.log(q11)
+console.log(q12)
+
+// ----------------------------------------------->
+
+// map()
+//filter()
+// forEach()
+// reduce()
+// every()
+// some()
+// find()
+// findIndex()
+
