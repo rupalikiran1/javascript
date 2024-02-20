7b73778ead5ea4eb245f768dbb4970f984c95161
rupalikiran1 <rupalikiran1999@gmail.com>
2024-02-20 10:43:45 +0530

    added

--- script9.js
+++ script9.js
@@ -61,6 +61,63 @@ let q12 = info.join("@")
 console.log(q11)
 console.log(q12)
 
+// ------------------------------>
+
+//program 1
+let birthyear = [1989,1990,1991,1992]
+let ages = []
+
+// [2024-1989,2024-1990,2024-1991,2024-1992]
+// [35,34,33,32]
+
+for(let i = 0 ; i < birthyear.length; i++){
+    //console.log(i)
+    // console.log(birthyear[i])
+    let x = 2024 - birthyear[i]
+     ages.push(x)
+}
+console.log(ages)
+
+// program 2
+
+let marks = [11,44,66,88,33,44,55]
+let above50 = []
+//[66,88,55]
+
+for(let i = 0 ; i < marks.length; i++)
+{
+    // console.log(i)
+    // console.log(marks[i])
+    if(marks[i] > 50){
+        // console.log(marks[i])
+        above50.push(marks[i])
+    }
+
+}
+console.log(above50)
+// program 3
+let numbers = [11,22,33]//66
+let total = 0
+for(let i = 0 ; i < numbers.length ; i++){
+    //console.log(i)
+    // console.log(numbers[i])
+    total = total + numbers[i]
+    //      0     +  11      ----->11
+    //       11   +  22  ------->33
+    //       33   +  33 ------>66
+      
+}
+console.log(total)
+
+// program 4
+
+let cities = ["nagpur","chennai","banglore","kolkata"]
+for(let i = 0 ; i < cities.length ; i++){
+    // console.log(i)
+    console.log("welcome" + cities[i])
+}
+
+
 // ----------------------------------------------->
 
 // map()
