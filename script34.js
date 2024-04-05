7896af51ce49e5fb866614b1281f63ab63556f4d
rupalikiran1 <rupalikiran1999@gmail.com>
2024-04-05 17:49:31 +0530

    added

---
+++ script34.js
@@ -0,0 +1,64 @@
+// let chinmay = {
+//  firstName:"chinmay",
+//  lastName:"deshpande",
+// displayName:function(){
+// 
+// }
+// let vijeet = {
+// firstName:"vijeet",
+// lastName:"dani"
+// }
+
+// let displayName2 = chinmay.displayName
+
+// let displayName2 = function(){
+//console.log(this.firstName + this.lastName)
+// }
+//displayName2()
+
+// bind
+// let q = displayName2.bind(vijeet)
+// let q = function(){
+// console.log(vijeet.firstName + vijeet.lastName)
+// }
+// }
+//q()
+
+// program 2
+
+let shivani = {
+    firstName:"shivani",
+    lastName:"hedau"
+}
+
+let hrushali = {
+    firstName:"hrushali",
+    lastName:"patil"
+
+}
+
+let vedant = {
+    firstName:"vedant",
+    lastName:"gaikwad"
+
+}
+let displayName4 = function(a){
+    console.log(this.firstName + this.lastName)
+    //console.log(vijeet)
+}
+displayName4.call(shivani,"hello")
+displayName4.call(hrushali,"hello")
+displayName4.call(vedant,"hello")
+
+// apply
+displayName4.apply(shivani,["bye","hi","bye","good bye","tc"])
+
+
+//let a = chinmay.displayName
+
+// let a = function(){
+// console.log(this.firstName + this.lastName)
+//}
+// a()
+// chinmay.a()
+//vijeet.a()
