c264e109a2bc253948e3ee23a9fff06285c0ee88
rupalikiran1 <rupalikiran1999@gmail.com>
2024-04-11 19:49:09 +0530

    added

--- AJAX/day3.js
+++ AJAX/day3.js
@@ -37,26 +37,76 @@
 //})
 
 // program 3
-let pro4 = new Promise(function (resolve, reject) {
-    let a = 10
-    let b = 5
-    if (a == b) {
-        resolve("hello")
+//let pro4 = new Promise(function (resolve, reject) {
+  //  let a = 10
+    //let b = 5
+    //if (a == b) {
+      //  resolve("hello")
+    //}
+    //else {
+      //  reject("bye")
+    //}
+//})
+  //  .then(function (str) {
+    //    console.log(str)
+      //  return "hello2"
+    //})
+    //.then(function (str2) {
+      //  console.log(str2)
+    //})
+    //.catch(function (str) {
+        //console.log(str)
+    //})
+    //.finally(function () {
+      //  console.log("I will Always execute")
+    //})
+
+    //-------------------------------------
+
+    function userCreated(){
+        return new Promise(function(resolve,reject){
+            setTimeout(function(){
+                resolve("user created")
+            },3000)
+        })
     }
-    else {
-        reject("bye")
+
+    function getId(){
+        return new Promise(function(resolve,reject){
+            setTimeout(function(){
+                resolve("get id")
+            },2000)
+        })
     }
-})
-    .then(function (str) {
-        console.log(str)
-        return "hello2"
-    })
-    .then(function (str2) {
-        console.log(str2)
-    })
-    .catch(function (str) {
-        console.log(str)
-    })
-    .finally(function () {
-        console.log("I will Always execute")
-    })
+
+    function getInfo(){
+        return new Promise(function(resolve,reject){
+            setTimeout(function(){
+                resolve("get info")
+            },1000)
+        })
+    }
+    // promise consume
+  //userCreated()
+  //.then(function(str){
+    //console.log(str)
+    //return getId()
+  //})
+  //.then(function(str){
+    //console.log(str)
+    //return getInfo()
+  //})
+
+  //.then(function(str){
+    //console.log(str)
+  //})
+
+async function getUserInfo(){
+    let a = await userCreated()
+    console.log(a)
+    let b = await getId()
+    console.log(b)
+    let c = await getInfo()
+    console.log(c)
+  }
+getUserInfo()
